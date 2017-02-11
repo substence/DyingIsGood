f7592dc95fe02ca8f29d0727e24716c84f6b4e9e
Orlando <substence@gmail.com>
2017-02-11 03:44:00 -0800

    Add finish waypoint

--- Source/DyingIsGood/Minion.cpp
+++ Source/DyingIsGood/Minion.cpp
@@ -3,6 +3,7 @@
 #include "DyingIsGood.h"
 #include "Minion.h"
 #include "DyingIsGoodAIController.h"
+#include "Runtime/Engine/Classes/Engine/TriggerBase.h"
 
 
 // Sets default values
@@ -29,10 +30,29 @@ void AMinion::BeginPlay()
 	AutoPossessAI = EAutoPossessAI::PlacedInWorldOrSpawned;
 	SpawnDefaultController();
 
+
 	FVector TargetPosition = GetActorLocation();
+	TArray<AActor*> FoundActors;
+	UGameplayStatics::GetAllActorsWithTag(GetWorld(), FName(TEXT("Finish")), FoundActors);
+	for (size_t i = 0; i < FoundActors.Num(); i++)
+	{
+		AActor* actor = FoundActors[i];
+		if (actor->Tags.Num() > 0)
+		{
+			UE_LOG(LogTemp, Warning, TEXT("Your message"));
+			//UE_LOG(LogTemp, Warning, TEXT("Your message %s"), *actor->Tags[0].ToString());
+		}
+
+		if (actor->ActorHasTag(FName(TEXT("Finish"))))
+		{
+			TargetPosition = actor->GetActorLocation();
+		}
+	}
+
+	/*FVector TargetPosition = GetActorLocation();
 	const float distance = 1000.0f;
 	TargetPosition.X += FMath::FRandRange(-distance, distance);
-	TargetPosition.Y += FMath::FRandRange(-distance, distance);
+	TargetPosition.Y += FMath::FRandRange(-distance, distance);*/
 	UWorld* World = GetWorld();
 	if (World)
 	{
